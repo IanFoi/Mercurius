ef0bfc3e6be7276102c7e7d68d9f2ca10d3782b6
Ian Arroyo <ian_arroyo@ciencias.unam.mx>
2024-08-29 23:01:51 -0600

    :wrench: modificando .gitignore y el sistema de construccion

--- src/server/server.c
+++ src/server/server.c
@@ -1,1 +1,15 @@
 #include<stidio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+#include<cJSON.h>
+
+#define PORT 12345
+
+typedef struct{
+  int socket;
+  char username[9];
+}
+
